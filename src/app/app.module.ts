31edc8c9fb6f2670c211fe39a7c4d043c09dbbd3
ibtihelbenabdallah <ibtihelbenabdallah94@gamil.com>
2024-11-21 15:08:10 +0100

    project angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
